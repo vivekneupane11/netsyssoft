362428e435cb6aabd051a97e3cb1cd75ffbc5fde
vivekneupane11 <vivekneupane11@gmail.com>
2023-12-19 23:38:55 +0545

    minor

--- src/components/Maps/index.tsx
+++ src/components/Maps/index.tsx
@@ -4,7 +4,7 @@ export default function MapView() {
     
 <section className="text-gray-600 body-font relative w-[1400px] h-[40vh] mt-12 overflow-hidden">
   <div className="absolute inset-0 bg-gray-300">
-    <iframe width="100%" height="100%" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" ></iframe>
+    <iframe width="100%" height="100%" title="map"  src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" ></iframe>
   </div>
  
 </section>
